b8415d04f7b20b326b0fe1db7d9aca34727a7d49
Joruno <pingciyuheye@gmail.com>
2026-09-11 14:01:10 +0800

    Fixed the comments of Exercise 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,7 +1,7 @@
 # mortgage.py
 # Dave has decided to take out a 30-year fixed rate mortgage of $500,000 with Guido’s Mortgage, Stock Investment, 
 # and Bitcoin trading corporation. The interest rate is 5% and the monthly payment is $2684.11.
-# Exercise 1.9
+# Exercise 1.10
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
